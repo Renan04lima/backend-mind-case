da06a8d7c3708aa9b02b648cc79ce95c930b57aa
renan04lima <renan04lima@outlook.com>
2024-04-21 11:35:35 -0300

    feat: add template method of controller and http helpers

---
+++ src/application/helpers/http.ts
@@ -0,0 +1,31 @@
+import { ServerError, UnauthorizedError } from "../errors/http"
+
+export type HttpResponse<T = any> = {
+    statusCode: number
+    data: T
+}
+
+export const ok = <T = any>(data: T): HttpResponse<T> => ({
+    statusCode: 200,
+    data
+})
+
+export const created = <T = any>(data: T): HttpResponse<T> => ({
+    statusCode: 201,
+    data
+})
+
+export const badRequest = (error: Error): HttpResponse<Error> => ({
+    statusCode: 400,
+    data: error
+})
+
+export const unauthorized = (): HttpResponse<Error> => ({
+    statusCode: 401,
+    data: new UnauthorizedError()
+})
+
+export const serverError = (error: unknown): HttpResponse<Error> => ({
+    statusCode: 500,
+    data: new ServerError(error)
+})
