eddbfcf2e1eecd38ef1d3e3237322f139c398553
renan04lima <renan04lima@outlook.com>
2024-04-23 16:29:17 -0300

    feat: add route GET api/products

--- src/main/factories/index.ts
+++ src/main/factories/index.ts
@@ -1,3 +1,4 @@
+import { ListProductsController } from '@/application/controllers/list-products'
 import { RegisterProductController } from '@/application/controllers/register-product'
 import { setupRegisterProduct } from '@/domain/use-cases/register-product'
 import { MySqlProductRepository } from '@/infra/mysql/repos/product-repo'
@@ -5,5 +6,11 @@ import { MySqlProductRepository } from '@/infra/mysql/repos/product-repo'
 export const makeRegisterProductController = () => {
   return new RegisterProductController(
     setupRegisterProduct(new MySqlProductRepository())
+  )
+}
+
+export const makeListProductsController = () => {
+  return new ListProductsController(
+    new MySqlProductRepository().list.bind(new MySqlProductRepository())
   )
 }
--- src/main/routes/real-estate.ts
+++ src/main/routes/real-estate.ts
@@ -1,9 +1,13 @@
 import { adaptExpressRoute as adapt } from '@/main/adapters/express-router'
 import { adaptMulter as upload } from '@/main/adapters/multer'
-import { makeRegisterProductController } from '@/main/factories'
+import {
+  makeRegisterProductController,
+  makeListProductsController,
+} from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/products', upload, adapt(makeRegisterProductController()))
+  router.get('/products', adapt(makeListProductsController()))
 }
